3e01afe23e4f4429167ebfaa1803142911e30aeb
LiaOTCO <rosalia.otero@gmail.com>
2023-06-15 11:56:05 +0200

    Dividido en 3 archivos sin formato ni funcionalidad oscuro ni ampliar botones funcionando y mostrando operaciones

--- funcionesCalc.js
+++ funcionesCalc.js
@@ -1,26 +1,34 @@
 function display(val) {
-    document.getElementById('result').value += val;
+  document.getElementById('result').innerHTML += val;
+}
+
+function clearScreen() {
+  document.getElementById('result').innerHTML = "";
+}
+
+function solve() {
+  let x = document.getElementById('result').innerHTML;
+  let y = eval(x);
+  document.getElementById('result').innerHTML = y;
+  return y;
+}
+function cambiaTema() {
+  alert(tema.value);
+      if ([tema="darknormal"]) {
+          document.documentElement.setAttribute('tema', 'darknormal');
+          
+      } else {
+          document.documentElement.setAttribute('tema', 'lightnormal');
+        
+      }
   }
-  
-  function clearScreen() {
-    document.getElementById('result').value = "";
+let imp = document.getElementsByClassName('claro');
+function modo() {
+  for (let i = 0; i < imp.length; i++) {
+      if (imp[i].className == 'claro' || imp[i].className == 'nada claro') {
+          imp[i].classList.add('oscuro');
+      } else {
+          imp[i].classList.remove('oscuro')
+      }
   }
-  
-  function solve() {
-    let x = document.getElementById('result').value;
-    let y = eval(x);
-    document.getElementById('result').value = y;
-    return y;
-  }
-   
-  
-    const colorSwitch = document.querySelector('#switchmodo input[type="checkbox"]');
-    function cambiaTema(ev){
-        if(ev.target.checked){
-            document.documentElement.setAttribute('tema', 'dark');
-        } else {
-            document.documentElement.setAttribute('tema', 'light');
-        }
-    }
-    colorSwitch.addEventListener('change', cambiaTema);
-  
+}
