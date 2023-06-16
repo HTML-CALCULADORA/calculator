b0adc346f09ff4476a359377ed960dd804f329b9
LiaOTCO <rosalia.otero@gmail.com>
2023-06-16 10:18:00 +0200

    añadidos botones

--- funcionesCalc.js
+++ funcionesCalc.js
@@ -32,3 +32,9 @@ function modo() {
       }
   }
 }
+/*Funcion para menú responsive*/
+function onMenuClick() {
+  var navbar = document.getElementById("navigation-bar");
+  var responsive_class_name = "responsive";
+  navbar.classList.toggle(responsive_class_name);
+}
