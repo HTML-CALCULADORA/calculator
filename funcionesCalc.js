ec265231999b455bb67627996b50ef2a271c50ae
LiaOTCO <rosalia.otero@gmail.com>
2023-06-16 13:02:17 +0200

    Funcionalidad aumento texto añadida pero sin verificar si modo noche o dia activo Botones funcionando

--- funcionesCalc.js
+++ funcionesCalc.js
@@ -13,16 +13,32 @@ function solve() {
   return y;
 }
 function cambiaTema(activo) {
+  let modo=document.getElementById('id-sun').toggle;
   if (activo=='moon') {
       document.documentElement.setAttribute('tema', 'darknormal');
-      document.getElementById('id-sun').classList.remove('active')
-      document.getElementById('id-moon').classList.add('active')     
+      document.getElementById('id-sun').classList.remove('active');
+      document.getElementById('id-moon').classList.add('active');    
   } else {
       document.documentElement.setAttribute('tema', 'lightnormal');
       document.getElementById('id-moon').classList.remove('active')
-      document.getElementById('id-sun').classList.add('active') 
+      document.getElementById('id-sun').classList.add('active'); 
   }
 }
+
+function cambiaTexto(activo){
+  if (activo=='lupaMax') {
+    document.documentElement.setAttribute('tema', 'lightgrande');
+    document.getElementById('id-lupaMin').classList.remove('active')
+    document.getElementById('id-lupaMax').classList.add('active'); 
+}    
+else {
+  document.documentElement.setAttribute('tema', 'lightnormal');
+  document.getElementById('id-lupaMax').classList.remove('active')
+  document.getElementById('id-lupaMin').classList.add('active');
+}
+}
+
+
 /*Funcion para menú responsive*/
 function onMenuClick() {
   var navbar = document.getElementById("navigation-bar");
